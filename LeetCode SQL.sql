d252dc2d017b460d153f6adf6541ea2aabd402ac
Kevin Riady <55337299+KevinRiady@users.noreply.github.com>
2020-08-29 19:12:21 -0400

    Update LeetCode SQL.sql

--- LeetCode SQL.sql
+++ LeetCode SQL.sql
@@ -1,4 +1,7 @@
+-- #175 Combine Two Tables
+-- Write a SQL query for a report that provides the following information for each person in the Person table, regardless if there is an address for each of those people:
 SELECT Person.FirstName, Person.LastName, Address.City, Address.State
 FROM Person
 LEFT JOIN Address
 ON Person.PersonId = Address.PersonId;
+
